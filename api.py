4ce01a694cffdd704b8e5b43b09e434190968654
rhino2rhonda <rhino2rhonda@gmail.com>
2018-08-20 16:36:09 +0530

    avoided loading pretrianed weights

--- api.py
+++ api.py
@@ -75,7 +75,8 @@ def fetch_map():
 
     # Configs
     train_config = train.TrainingConfig.load_from_file(request_config.train_config_path)
-    train_config.model_params['pretrained'] = False  # avoid loading pre-trained weights
+    if train_config.model == 'cnn_classifier':  # avoid loading pre-trained weights
+        train_config.model_params['pretrained'] = False
     dataset_config = dp.DataPrepConfig.load_from_dataset(train_config.dataset_path)
     dataset_mode = dp.read_h5_attrib('mode', dataset_config.get_dataset_path())
 
