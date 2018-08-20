a3c5ab60b7764afdce1d7add3d3879db573a38ca
rhino2rhonda <rhino2rhonda@gmail.com>
2018-08-20 11:00:00 +0530

    Updated readme

--- demo/src/viz/all.js
+++ demo/src/viz/all.js
@@ -4,12 +4,15 @@ import VizCrystals from "./viz-crystals.js";
 import VizPointCloud from "./viz-point-cloud.js";
 import VizPsychedelic from "./viz-psychedelic";
 import VizSaturn from "./viz-saturn";
+import VizStorm from "./viz-storm";
 
+// Viz code to class map
 export const vizMap = {
   'viz-bar': VizBar,
   'viz-real': VizReal,
   'viz-crystals': VizCrystals,
   'viz-point-cloud': VizPointCloud,
   'viz-psychedelic': VizPsychedelic,
-  'viz-saturn': VizSaturn
+  'viz-saturn': VizSaturn,
+  'viz-storm': VizStorm,
 };
